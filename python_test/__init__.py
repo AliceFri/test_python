6a5ba7a1773feddcb8ff64b429a0dbbe53d57018
quanxipeng <quanxipeng@autox.ai>
2022-10-14 18:51:08 +0800

    _t

---
+++ python_test/__init__.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    i = 1
+
+    while i != 3:
+        i = input()
+        i = int(i)
+
+    import a
+    print('done')
---
+++ python_test/a.py
@@ -0,0 +1,7 @@
+print('---- into a')
+import b
+a1 = 1
+
+
+def a2():
+    pass
